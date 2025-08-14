8809733ba689a374ae64c72db46f1ea9444c8a2c
Jhon P <j.a.pr12@hotmail.com>
2025-08-14 17:53:27 -0400

    mensaje del cambio

--- src/components/Mapa.jsx
+++ src/components/Mapa.jsx
@@ -63,7 +63,7 @@ const Mapa = () => {
           >
             <div className="aspect-video bg-gray-200 flex items-center justify-center">
               <iframe
-                src="https://www.openstreetmap.org/export/embed.html?bbox=-70.7000,-18.5000,-70.6000,-18.4000&layer=mapnik&marker=19.326133, -69.469031"
+                src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d3891.5181380551185!2d-69.46951754504505!3d19.32550417518583!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e1!3m2!1ses-419!2sdo!4v1755208333241!5m2!1ses-419!2sdo"
                 width="100%"
                 height="100%"
                 style={{ border: 0, minHeight: '400px' }}
